325187dcd004cc8320a3b62b4fc4884ea2efb7a1
Usman Kabeer <uahmad565565@gmail.com>
2025-12-01 03:20:21 +0500

    changed AuthorizationLevel for az function

--- tools-serverless-api/Function1.cs
+++ tools-serverless-api/Function1.cs
@@ -1,23 +1,36 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace tools_serverless_api;
 
 public class Function1
 {
+    private readonly IConfiguration _config;
     private readonly ILogger<Function1> _logger;
 
-    public Function1(ILogger<Function1> logger)
+    public Function1(ILogger<Function1> logger, IConfiguration configuration)
     {
+        this._config = configuration;
         _logger = logger;
     }
 
     [Function("Function1")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions! endpoint working");
+        string str = "";
+        try
+        {
+            str = _config["apikey"] ?? "Not Found";
+        }
+        catch (System.Exception ex)
+        {
+            _logger.LogError($"Error retrieving configuration: {ex.Message}");
+            str = "Error retrieving configuration";
+        }
+        return new OkObjectResult("Welcome to Azure Functions! endpoint working/ " + str);
     }
 }
